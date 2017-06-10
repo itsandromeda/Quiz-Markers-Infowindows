b99ef0da0d5bb6035262154c197632c023e3b127
itsandromeda <itsandromeda@gmail.com>
2017-06-10 16:08:42 -0500

    Marker

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,24 @@
-// Create a map variable
-var map;
-// Function to initialize the map within the map div
+/*global document, google*/
+var map, marker, singleLatLng;
+
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 40.74135,
-            lng: -73.99802
-        },
-        zoom: 14
-    });
+    "use strict";
     // Create a single latLng literal object.
-    var singleLatLng = {
+    singleLatLng = {
         lat: 40.74135,
         lng: -73.99802
     };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: singleLatLng,
+        zoom: 14
+    });
+    marker = new google.maps.Marker({
+        position: singleLatLng,
+        map: map,
+        title: 'This is a marker!'
+    });
+
+
     // TODO: Create a single marker appearing on initialize -
     // Create it with the position of the singleLatLng,
     // on the map, and give it your own title!
